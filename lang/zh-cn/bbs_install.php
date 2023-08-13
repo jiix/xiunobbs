fc5aab9cc5157944e4fa40e8d05f759f25b6547f
jiix <hello@jiih.com>
2023-08-13 15:57:13 +0800

    Update bbs_install.php

--- lang/zh-cn/bbs_install.php
+++ lang/zh-cn/bbs_install.php
@@ -40,7 +40,7 @@ return array(
 	'admin_pw' => '管理员密码',
 	'installing_about_moment' => '正在安装，大概需要一分钟左右',
 	'license_title' => 'Xiuno BBS 4.0 中文版授权协议',
-	'license_content' => 'Xiuno BBS 4.0 采用 MIT 协议发布，您可以自由修改、派生版本、商用而不用担心任何法律风险（修改后应保留原来的版权信息）。',
+	'license_content' => 'Xiuno BBS 4.0 采用 MIT 协议发布，您可以自由修改、派生版本、商用而不用担心任何法律风险。',
 	'license_date' => '发布时间：2018年1月22日',
 	'agree_license_to_continue' => '同意协议继续安装',
 	'install_title' => 'Xiuno BBS 4.0 安装向导',
